fc2d62a684e9bfb631a19d3fd58056d2e28a32f3
Kylie Benfield <kylie@revelryplay.com>
2024-12-14 14:19:09 -0700

    Further build-libs script work

--- build-libs.sh
+++ build-libs.sh
@@ -2,7 +2,7 @@
 # copy the output .a, .so, and .lib files to the lib directory of the Net Toys, OpenGL/Imgui, OpenGL/UI-Basics, and Threaded directories
 # copy the files in ArgoDraft/include directory to the Net Toys, OpenGL/Imgui, OpenGL/UI-Basics, and Threaded include/ArgoDraft directories
 
-projectDirectories=("Net\ Toys" "OpenGL/Imgui" "OpenGL/UI-Basics" "Threaded")
+projectDirectories=("NetToys" "OpenGL/Imgui" "OpenGL/UI-Basics" "Threaded")
 currentDirectory=$(pwd)
 
 cd ArgoDraft
@@ -16,22 +16,29 @@ cmake --build build/release
 # Remove the existing ArgoDraft lib files and include/ArgoDraft directories in the project directories
 for projectDirectory in "${projectDirectories[@]}"
 do
-    rm -rf $currentDirectory/$projectDirectory/lib/ArgoDraft.*
-    rm -rf $currentDirectory/$projectDirectory/include/ArgoDraft
+    rm -rf $currentDirectory/$projectDirectory/lib/ArgoDraft.* || true
+    rm -rf $currentDirectory/$projectDirectory/include/ArgoDraft || true
 done
 
 # Create the include/ArgoDraft directories in the project directories
 for projectDirectory in "${projectDirectories[@]}"
 do
-    mkdir $currentDirectory/$projectDirectory/include/ArgoDraft
+    mkdir $currentDirectory/$projectDirectory/include || true
+    mkdir $currentDirectory/$projectDirectory/include/ArgoDraft || true
+done
+
+# Create the lib directories in the project directories
+for projectDirectory in "${projectDirectories[@]}"
+do
+    mkdir $currentDirectory/$projectDirectory/lib || true
 done
 
 # Copy the ArgoDraft lib files and include/ArgoDraft directories to the project directories
 for projectDirectory in "${projectDirectories[@]}"
 do
-    cp -r $currentDirectory/ArgoDraft/build/debug/libArgoDraft.* $currentDirectory/$projectDirectory/lib
-    cp -r $currentDirectory/ArgoDraft/build/debug/ArgoDraft.* $currentDirectory/$projectDirectory/lib
-    cp -r $currentDirectory/ArgoDraft/include/* $currentDirectory/$projectDirectory/include/ArgoDraft
+    cp -r $currentDirectory/ArgoDraft/build/debug/libArgoDraft.* $currentDirectory/$projectDirectory/lib/ || true
+    cp -r $currentDirectory/ArgoDraft/build/debug/ArgoDraft.* $currentDirectory/$projectDirectory/lib/ || true
+    cp -r $currentDirectory/ArgoDraft/include/* $currentDirectory/$projectDirectory/include/ArgoDraft/  || true
 done
 
 cd $currentDirectory
