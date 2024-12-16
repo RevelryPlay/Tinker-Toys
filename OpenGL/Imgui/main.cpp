0ae10a17b35f2f91c2eee6e64ce73715e2f2b47c
Kylie Benfield <kylie@revelryplay.com>
2024-12-15 22:27:16 -0700

    Adding OpenGL - ImGUI project

--- OpenGL/Imgui/main.cpp
+++ OpenGL/Imgui/main.cpp
@@ -1,7 +1,20 @@
 #include <iostream>
+#include <ArgoDraft/ArgoLogger.hpp>
+
+#include "src/imguiRenderer.cpp"
 
 int main() {
-    std::cout << "Hello World!" << std::endl;
+    try {
+        ImguiRenderer::renderOpenGL();
+    }
+    catch (const std::exception& e) {
+        
+        const std::string logFileName = "default.log";
+        const auto logger = ArgoDraft::Logger(logFileName);
+        logger.LogMessage(("Error: " + std::string(e.what())).c_str(), ArgoDraft::LogLevel::CRITICAL);
+
+        return 1;
+    }
 
     return 0;
 }
